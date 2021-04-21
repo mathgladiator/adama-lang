f5dba1237ad9ce1ed0cd8ff3b9b99817b47ef811
mathgladiator <primary@mathgladiator.com>
2021-04-20 22:47:40 -0700

    massive touch; phase 2 of refactor
    new copyright notice for all new files and old; we are in 2021

--- core/src/test/java/org/adamalang/translator/env/CompilerOptionsTests.java
+++ core/src/test/java/org/adamalang/translator/env/CompilerOptionsTests.java
@@ -1,6 +1,12 @@
-/* The Adama Programming Language For Board Games!
- *    See http://www.adama-lang.org/ for more information.
- * (c) copyright 2020 Jeffrey M. Barber (http://jeffrey.io) */
+/*
+ * This file is subject to the terms and conditions outlined in the file 'LICENSE'
+ * which is in the root directory of the repository. This file is part of the 'Adama'
+ * project which is a programming language and document store for board games.
+ * 
+ * See http://www.adama-lang.org/ for more information.
+ * 
+ * (c) 2020 - 2021 by Jeffrey M. Barber (http://jeffrey.io)
+*/
 package org.adamalang.translator.env;
 
 import org.junit.Assert;
--- devkit/src/main/java/org/adamalang/Copyright.java
+++ devkit/src/main/java/org/adamalang/Copyright.java
@@ -1,18 +1,50 @@
+/*
+ * This file is subject to the terms and conditions outlined in the file 'LICENSE'
+ * which is in the root directory of the repository. This file is part of the 'Adama'
+ * project which is a programming language and document store for board games.
+ * 
+ * See http://www.adama-lang.org/ for more information.
+ * 
+ * (c) 2020 - 2021 by Jeffrey M. Barber (http://jeffrey.io)
+*/
 package org.adamalang;
 
 import java.io.File;
+import java.nio.file.Files;
 
 public class Copyright {
-  private static void scan(File root) {
+  public static final String NEW_COPYRIGHT_MESSAGE = "/*\n" +
+          " * This file is subject to the terms and conditions outlined in the file 'LICENSE'\n" +
+          " * which is in the root directory of the repository. This file is part of the 'Adama'\n" +
+          " * project which is a programming language and document store for board games.\n" +
+          " * \n" +
+          " * See http://www.adama-lang.org/ for more information.\n" +
+          " * \n" +
+          " * (c) 2020 - 2021 by Jeffrey M. Barber (http://jeffrey.io)\n" +
+          "*/\n";
+
+  private static void scan(File root) throws Exception {
     for(File f : root.listFiles()) {
       if (f.isDirectory()) {
         scan(f);
       } else {
+        if(f.getName().endsWith(".java")) {
+          String code = Files.readString(f.toPath());
+          int start = code.indexOf("/*");
+          int end = code.indexOf("*/");
+          if (start >= 0 && start <= 5 && end > start) {
+            String newCode = NEW_COPYRIGHT_MESSAGE + code.substring(end + 2).trim() + "\n";
+            Files.writeString(f.toPath(), newCode);
+          } else {
+            String newCode = NEW_COPYRIGHT_MESSAGE + code.trim() + "\n";
+            Files.writeString(f.toPath(), newCode);
+          }
+        }
       }
     }
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     File f = new File(".");
     scan(f);
   }
