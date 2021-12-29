b39d10a14a91f310aa8d6acb64adc7854ef36c4b
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2021-12-29 05:45:31 -0600

    tune copyright

--- cli/src/main/java/org/adamalang/cli/commands/Contrib.java
+++ cli/src/main/java/org/adamalang/cli/commands/Contrib.java
@@ -1,11 +1,10 @@
 /*
- * This file is subject to the terms and conditions outlined in the file 'LICENSE'
- * The 'LICENSE' file is in the root directory of the repository. Hint: it is MIT.
- * 
+ * This file is subject to the terms and conditions outlined in the file 'LICENSE' (hint: it's MIT); this file is located in the root directory near the README.md which you should also read.
+ *
  * This file is part of the 'Adama' project which is a programming language and document store for board games; however, it can be so much more.
- * 
+ *
  * See http://www.adama-lang.org/ for more information.
- * 
+ *
  * (c) 2020 - 2022 by Jeffrey M. Barber (http://jeffrey.io)
 */
 package org.adamalang.cli.commands;
@@ -59,13 +58,12 @@ public class Contrib {
     }
 
     public static final String NEW_COPYRIGHT_MESSAGE = "/*\n" +
-            " * This file is subject to the terms and conditions outlined in the file 'LICENSE'\n" +
-            " * The 'LICENSE' file is in the root directory of the repository. Hint: it is MIT.\n" +
-            " * \n" +
+            " * This file is subject to the terms and conditions outlined in the file 'LICENSE' (hint: it's MIT); this file is located in the root directory near the README.md which you should also read.\n" +
+            " *\n" +
             " * This file is part of the 'Adama' project which is a programming language and document store for board games; however, it can be so much more.\n" +
-            " * \n" +
+            " *\n" +
             " * See http://www.adama-lang.org/ for more information.\n" +
-            " * \n" +
+            " *\n" +
             " * (c) 2020 - 2022 by Jeffrey M. Barber (http://jeffrey.io)\n" +
             "*/\n";
 
