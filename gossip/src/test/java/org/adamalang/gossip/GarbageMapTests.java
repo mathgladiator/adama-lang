826a830e709bbf84e7c7e37e651e8ebd7ce427c9
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2021-12-29 18:40:56 -0600

    100% coverage for GCMap

--- gossip/src/test/java/org/adamalang/gossip/GarbageMapTests.java
+++ gossip/src/test/java/org/adamalang/gossip/GarbageMapTests.java
@@ -19,6 +19,7 @@ public class GarbageMapTests {
     for (int k = 0; k < 100; k++) {
       map.put("k+" + k, "v:" + k, 0);
     }
+    Assert.assertEquals(10, map.size());
   }
 
   @Test
@@ -28,12 +29,12 @@ public class GarbageMapTests {
     map.put("x", "f(x)", 0);
     Assert.assertEquals(1, map.keys().size());
     Assert.assertEquals("f(x)", map.get("x"));
-    Assert.assertEquals(1, map.keys().size());
+    Assert.assertEquals(1, map.size());
     Assert.assertEquals("f(x)", map.remove("x"));
-    Assert.assertEquals(0, map.keys().size());
+    Assert.assertEquals(0, map.size());
     map.put("x", "f(x)", 0);
     Assert.assertEquals(0, map.gc(0));
-    Assert.assertEquals(1, map.keys().size());
+    Assert.assertEquals(1, map.size());
     Assert.assertEquals("f(x)", map.get("x"));
     Assert.assertEquals(0, map.gc(Constants.MILLISECONDS_TO_SIT_IN_GARBAGE_MAP - 1));
     Assert.assertEquals(1, map.keys().size());
