b4ec6f6a6207573aae6266c179ab5432057ed9c2
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2022-01-17 04:05:59 -0600

    clean up grpc

--- grpc/make-ca.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-SUBJ="/C=US/ST=Kansas/L=KansasCity/O=Adama/OU=Adama/CN=adama.com/emailAddress=admin@adama.com"
-
-# make ca-cert.pem for client and server to use
-mkdir ca
-openssl req -x509 -newkey rsa:4096 -days 365 -nodes -keyout ca/ca-key.pem -out ca/ca-cert.pem -subj "$SUBJ"
--- grpc/make-localhost.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -jar target/grpc-0.2-jar-with-dependencies.jar --ca ca --ip 127.0.0.1 --out localhost.identity
