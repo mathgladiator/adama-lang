6220d0b00420080ba909f3cbeaeb38cb5782f5d4
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2022-01-22 04:40:15 -0600

    Update ProtectedUUID.java

--- saas/src/main/java/org/adamalang/extern/ProtectedUUID.java
+++ saas/src/main/java/org/adamalang/extern/ProtectedUUID.java
@@ -44,7 +44,7 @@ public class ProtectedUUID {
         sb.append(UUID_CODEC_BASE[(int) (trailer % m)]);
         trailer /= m;
       }
-      MessageDigest md = MessageDigest.getInstance("SHA256");
+      MessageDigest md = MessageDigest.getInstance("MD5");
       byte[] digest = md.digest(sb.toString().getBytes(StandardCharsets.UTF_8));
       sb.append('X');
       v = Math.abs(digest[0] + digest[1] * 256 + digest[2] * 256 * 256);
