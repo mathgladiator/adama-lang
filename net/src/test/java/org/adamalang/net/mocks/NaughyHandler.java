e3f605a47f574e102932288d27f5e010f4460583
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2022-03-05 14:39:26 -0600

    copyright

--- net/src/test/java/org/adamalang/net/mocks/NaughyHandler.java
+++ net/src/test/java/org/adamalang/net/mocks/NaughyHandler.java
@@ -1,3 +1,12 @@
+/*
+ * This file is subject to the terms and conditions outlined in the file 'LICENSE' (hint: it's MIT); this file is located in the root directory near the README.md which you should also read.
+ *
+ * This file is part of the 'Adama' project which is a programming language and document store for board games; however, it can be so much more.
+ *
+ * See http://www.adama-lang.org/ for more information.
+ *
+ * (c) 2020 - 2022 by Jeffrey M. Barber (http://jeffrey.io)
+ */
 package org.adamalang.net.mocks;
 
 import io.netty.buffer.ByteBuf;
