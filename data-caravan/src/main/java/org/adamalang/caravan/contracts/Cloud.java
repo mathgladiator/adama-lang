44430828bd961e956dd5e2cd388b02913d509603
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2022-05-24 12:54:23 -0500

    sketch out cloud contract

---
+++ data-caravan/src/main/java/org/adamalang/caravan/contracts/Cloud.java
@@ -0,0 +1,23 @@
+/*
+ * This file is subject to the terms and conditions outlined in the file 'LICENSE' (hint: it's MIT); this file is located in the root directory near the README.md which you should also read.
+ *
+ * This file is part of the 'Adama' project which is a programming language and document store for board games; however, it can be so much more.
+ *
+ * See http://www.adama-lang.org/ for more information.
+ *
+ * (c) 2020 - 2022 by Jeffrey M. Barber (http://jeffrey.io)
+ */
+package org.adamalang.caravan.contracts;
+
+import org.adamalang.common.Callback;
+
+import java.io.File;
+
+/** restore/backup from the cloud */
+public interface Cloud {
+  /** restore the archive key from the cloud to a local file */
+  public void restore(String archiveKey, Callback<File> callback);
+
+  /** backup the given file and send to the cloud */
+  public void backup(File archiveFile, Callback<Void> callback);
+}
