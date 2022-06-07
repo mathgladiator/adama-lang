092d40e34e4a20b9812e6b246a90ac47a2b4f218
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2022-06-06 19:19:56 -0500

    introduce DocumentSnapshot as a new class

---
+++ core/src/main/java/org/adamalang/runtime/data/DocumentSnapshot.java
@@ -0,0 +1,23 @@
+/*
+ * This file is subject to the terms and conditions outlined in the file 'LICENSE' (hint: it's MIT); this file is located in the root directory near the README.md which you should also read.
+ *
+ * This file is part of the 'Adama' project which is a programming language and document store for board games; however, it can be so much more.
+ *
+ * See http://www.adama-lang.org/ for more information.
+ *
+ * (c) 2020 - 2022 by Jeffrey M. Barber (http://jeffrey.io)
+ */
+package org.adamalang.runtime.data;
+
+/** a snapshot of a document at a specific sequencer */
+public class DocumentSnapshot {
+  public final int seq;
+  public final String json;
+  public final int history;
+
+  public DocumentSnapshot(int seq, String json, int history) {
+    this.seq = seq;
+    this.json = json;
+    this.history = history;
+  }
+}
