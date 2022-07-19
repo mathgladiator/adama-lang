fc5c4b69277f1e14811ddc167c6f64c0a211ae9e
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2022-07-19 10:32:14 -0500

    Add reflection to LibAdama

--- core/src/main/java/org/adamalang/runtime/stdlib/LibAdama.java
+++ core/src/main/java/org/adamalang/runtime/stdlib/LibAdama.java
@@ -9,6 +9,7 @@
  */
 package org.adamalang.runtime.stdlib;
 
+import org.adamalang.runtime.json.JsonStreamWriter;
 import org.adamalang.runtime.natives.NtDynamic;
 import org.adamalang.translator.env.CompilerOptions;
 import org.adamalang.translator.env.EnvironmentState;
@@ -37,5 +38,25 @@ public class LibAdama {
     } catch (AdamaLangException ale) {
       return new NtDynamic("[{\"error\":true}]");
     }
+  }
+
+  public static NtDynamic reflect(String code) {
+    try {
+      final var options = CompilerOptions.start().make();
+      final var globals = GlobalObjectPool.createPoolWithStdLib();
+      final var state = new EnvironmentState(globals, options);
+      final var document = new Document();
+      document.setClassName("Validate");
+      final var tokenEngine = new TokenEngine("<direct code>", code.codePoints().iterator());
+      final var parser = new Parser(tokenEngine);
+      parser.document().accept(document);
+      if (!document.check(state)) {
+        JsonStreamWriter writer = new JsonStreamWriter();
+        document.writeTypeReflectionJson(writer);
+        return new NtDynamic(writer.toString());
+      }
+    } catch (AdamaLangException ale) {
+    }
+    return new NtDynamic("null");
   }
 }
