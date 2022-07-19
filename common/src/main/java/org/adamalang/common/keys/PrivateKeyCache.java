46c26f04c1c112e8f6f8de92ab07b57d46538661
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2022-07-18 20:16:23 -0500

    Cache private keys in memory

---
+++ common/src/main/java/org/adamalang/common/keys/PrivateKeyCache.java
@@ -0,0 +1,82 @@
+/*
+ * This file is subject to the terms and conditions outlined in the file 'LICENSE' (hint: it's MIT); this file is located in the root directory near the README.md which you should also read.
+ *
+ * This file is part of the 'Adama' project which is a programming language and document store for board games; however, it can be so much more.
+ *
+ * See http://www.adama-lang.org/ for more information.
+ *
+ * (c) 2020 - 2022 by Jeffrey M. Barber (http://jeffrey.io)
+ */
+package org.adamalang.common.keys;
+
+import org.adamalang.ErrorCodes;
+import org.adamalang.common.Callback;
+import org.adamalang.common.ErrorCodeException;
+import org.adamalang.common.NamedRunnable;
+import org.adamalang.common.SimpleExecutor;
+
+import java.security.PrivateKey;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
+/** store private keys in memory for nearly forever. TODO: expire at some point... maybe */
+public abstract class PrivateKeyCache {
+  private final ConcurrentHashMap<SpaceKeyIdPair, PrivateKey> keys;
+  private final SimpleExecutor executor;
+
+  public PrivateKeyCache(SimpleExecutor executor) {
+    this.keys = new ConcurrentHashMap<>();
+    this.executor = executor;
+  }
+
+  protected abstract PrivateKey find(SpaceKeyIdPair pair);
+
+  public void get(String space, int keyId, Callback<PrivateKey> callback) {
+    SpaceKeyIdPair pair = new SpaceKeyIdPair(space, keyId);
+    PrivateKey immediate = keys.get(pair);
+    if (immediate != null) {
+      callback.success(immediate);
+      return;
+    }
+    executor.execute(new NamedRunnable("find-private-key") {
+      @Override
+      public void execute() throws Exception {
+        PrivateKey result = keys.get(pair);
+        if (result != null) {
+          callback.success(result);
+          return;
+        }
+        result = find(pair);
+        if (result == null) {
+          callback.failure(new ErrorCodeException(ErrorCodes.PRIVATE_KEY_NOT_FOUND));
+          return;
+        }
+        keys.put(pair, result);
+        callback.success(result);
+      }
+    });
+  }
+
+  public static class SpaceKeyIdPair {
+    public final String space;
+    public final int id;
+
+    public SpaceKeyIdPair(String space, int id) {
+      this.space = space;
+      this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      SpaceKeyIdPair that = (SpaceKeyIdPair) o;
+      return id == that.id && Objects.equals(space, that.space);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(space, id);
+    }
+  }
+}
---
+++ common/src/test/java/org/adamalang/common/keys/PrivateKeyCacheTests.java
@@ -0,0 +1,114 @@
+/*
+ * This file is subject to the terms and conditions outlined in the file 'LICENSE' (hint: it's MIT); this file is located in the root directory near the README.md which you should also read.
+ *
+ * This file is part of the 'Adama' project which is a programming language and document store for board games; however, it can be so much more.
+ *
+ * See http://www.adama-lang.org/ for more information.
+ *
+ * (c) 2020 - 2022 by Jeffrey M. Barber (http://jeffrey.io)
+ */
+package org.adamalang.common.keys;
+
+import org.adamalang.common.Callback;
+import org.adamalang.common.ErrorCodeException;
+import org.adamalang.common.NamedRunnable;
+import org.adamalang.common.SimpleExecutor;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class PrivateKeyCacheTests {
+  @Test
+  public void flow() throws Exception {
+    ArrayList<NamedRunnable> runnables = new ArrayList<>();
+    KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("DSA");
+    PrivateKey one = keyPairGen.generateKeyPair().getPrivate();
+    CountDownLatch latch = new CountDownLatch(4);
+    PrivateKeyCache cache = new PrivateKeyCache(new SimpleExecutor() {
+      @Override
+      public void execute(NamedRunnable command) {
+        runnables.add(command);
+      }
+
+      @Override
+      public Runnable schedule(NamedRunnable command, long milliseconds) {
+        return null;
+      }
+
+      @Override
+      public Runnable scheduleNano(NamedRunnable command, long nanoseconds) {
+        return null;
+      }
+
+      @Override
+      public CountDownLatch shutdown() {
+        return null;
+      }
+    }) {
+      @Override
+      protected PrivateKey find(PrivateKeyCache.SpaceKeyIdPair pair) {
+        if (pair.space.equals("one")) {
+          return one;
+        }
+        return null;
+      }
+    };
+    cache.get("one", 1, new Callback<PrivateKey>() {
+      @Override
+      public void success(PrivateKey value) {
+        Assert.assertNotNull(value);
+        latch.countDown();
+      }
+
+      @Override
+      public void failure(ErrorCodeException ex) {
+        Assert.fail();
+      }
+    });
+    cache.get("one", 1, new Callback<PrivateKey>() {
+      @Override
+      public void success(PrivateKey value) {
+        Assert.assertNotNull(value);
+        latch.countDown();
+      }
+
+      @Override
+      public void failure(ErrorCodeException ex) {
+        Assert.fail();
+      }
+    });
+    cache.get("space", 1, new Callback<PrivateKey>() {
+      @Override
+      public void success(PrivateKey value) {
+
+      }
+
+      @Override
+      public void failure(ErrorCodeException ex) {
+        Assert.assertEquals(643100, ex.code);
+        latch.countDown();
+      }
+    });
+    while (runnables.size() > 0) {
+      runnables.remove(0).run();
+    }
+    cache.get("one", 1, new Callback<PrivateKey>() {
+      @Override
+      public void success(PrivateKey value) {
+        Assert.assertNotNull(value);
+        latch.countDown();
+      }
+
+      @Override
+      public void failure(ErrorCodeException ex) {
+        Assert.fail();
+      }
+    });
+    Assert.assertTrue(latch.await(5000, TimeUnit.MILLISECONDS));
+  }
+}
