d9f044eec2622d3257df2c7334ca362a35961b31
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2022-07-19 12:45:04 -0500

    Update TokenTests.java

--- core/src/test/java/org/adamalang/translator/parser/token/TokenTests.java
+++ core/src/test/java/org/adamalang/translator/parser/token/TokenTests.java
@@ -38,7 +38,7 @@ public class TokenTests {
   public void coverageHashcode() {
     Token a1 = Token.WRAP("a");
     Token a2 = Token.WRAP("a");
-    Assert.assertEquals(977085218, a1.hashCode());
+    Assert.assertEquals(-293309790, a1.hashCode());
     Assert.assertEquals(a1.hashCode(), a2.hashCode());
   }
 
