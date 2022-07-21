6b5d5508752e49e048ea1ecae8c99075c68c0db6
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2022-07-21 12:57:06 -0500

    implement json delta integration
    This is a key part of linking documents into Adama

---
+++ core/src/main/java/org/adamalang/runtime/json/JsonDeltaIntegrate.java
@@ -0,0 +1,108 @@
+/*
+ * This file is subject to the terms and conditions outlined in the file 'LICENSE' (hint: it's MIT); this file is located in the root directory near the README.md which you should also read.
+ *
+ * This file is part of the 'Adama' project which is a programming language and document store for board games; however, it can be so much more.
+ *
+ * See https://www.adama-platform.com/ for more information.
+ *
+ * (c) 2020 - 2022 by Jeffrey M. Barber ( http://jeffrey.io )
+ */
+package org.adamalang.runtime.json;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JsonDeltaIntegrate {
+  /** implements the core integration algorithm that deltas emit */
+  public static void integrateDeltaIntoRoot(HashMap<String, Object> root, HashMap<String, Object> delta) {
+    for (Map.Entry<String, Object> entry : delta.entrySet()) {
+      String secretKey = "#" + entry.getKey();
+      Object prior = root.get(entry.getKey());
+      Object secretValue = root.get(secretKey);
+      Object next = entry.getValue();
+      if (next == null) {
+        if (prior != null) {
+          root.remove(secretKey);
+        }
+        root.remove(entry.getKey());
+      } else {
+        if (next instanceof HashMap) {
+          HashMap<String, Object> nextMap = (HashMap<String, Object>) next;
+          boolean arr = prior instanceof ArrayList || nextMap.containsKey("@o") || nextMap.containsKey("@s");
+          if (arr) {
+            if (prior == null) {
+              prior = new ArrayList<>();
+              root.put(entry.getKey(), prior);
+            }
+            if (secretValue == null) {
+              HashMap secretMap = new HashMap<>();
+              if (nextMap.containsKey("@o")) {
+                secretMap.put("@o", true);
+              }
+              secretMap.put("__key", entry.getKey());
+              secretValue = secretMap;
+              root.put(secretKey, secretMap);
+            }
+            HashMap<String, Object> base = (HashMap<String, Object>) secretValue;
+            ArrayList<Object> ordering = null;
+            Integer resize = null;
+            for (Map.Entry<String, Object> nextEntry : nextMap.entrySet()) {
+              Object val = nextEntry.getValue();
+              if ("@o".equals(nextEntry.getKey())) {
+                ordering = (ArrayList<Object>) val;
+              } else if ("@s".equals(nextEntry.getKey())) {
+                resize = (Integer) val;
+              } else if (val == null) {
+                base.remove(nextEntry.getKey());
+              } else {
+                if (val instanceof HashMap) {
+                  HashMap<String, Object> baseItem = (HashMap<String, Object>) base.get(nextEntry.getKey());
+                  if (baseItem == null) {
+                    baseItem = new HashMap<>();
+                    baseItem.put("__key", nextEntry.getKey());
+                    base.put(nextEntry.getKey(), baseItem);
+                  }
+                  integrateDeltaIntoRoot(baseItem, (HashMap<String, Object>) val);
+                } else {
+                  base.put(nextEntry.getKey(), val);
+                }
+              }
+            }
+            if (ordering != null) {
+              ArrayList<Object> priorArr = (ArrayList<Object>) prior;
+              ArrayList<Object> after = new ArrayList<>();
+              for (Object instr : ordering) {
+                if (instr instanceof Integer || instr instanceof String) {
+                  after.add(base.get("" + instr));
+                } else if (instr instanceof ArrayList) {
+                  int start = ((ArrayList<Integer>) instr).get(0);
+                  int end = ((ArrayList<Integer>) instr).get(1);
+                  for (int j = start; j <= end; j++) {
+                    after.add(priorArr.get(j));
+                  }
+                }
+              }
+              root.put(entry.getKey(), after);
+            } else if (resize != null) {
+              ArrayList<Object> priorArr = (ArrayList<Object>) prior;
+              ArrayList<Object> after = new ArrayList<>();
+              for (int k = 0; k < resize; k++) {
+                after.add(priorArr.get(k));
+              }
+              root.put(entry.getKey(), after);
+            }
+          } else {
+            if (prior == null || !(prior instanceof HashMap)) {
+              prior = new HashMap<>();
+              root.put(entry.getKey(), prior);
+            }
+            integrateDeltaIntoRoot((HashMap<String, Object>) prior, nextMap);
+          }
+        } else {
+          root.put(entry.getKey(), next);
+        }
+      }
+    }
+  }
+}
---
+++ core/src/test/java/org/adamalang/runtime/json/JsonDeltaIntegrateTests.java
@@ -0,0 +1,81 @@
+/*
+ * This file is subject to the terms and conditions outlined in the file 'LICENSE' (hint: it's MIT); this file is located in the root directory near the README.md which you should also read.
+ *
+ * This file is part of the 'Adama' project which is a programming language and document store for board games; however, it can be so much more.
+ *
+ * See https://www.adama-platform.com/ for more information.
+ *
+ * (c) 2020 - 2022 by Jeffrey M. Barber ( http://jeffrey.io )
+ */
+package org.adamalang.runtime.json;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+public class JsonDeltaIntegrateTests {
+
+  private HashMap<String, Object> of(String x) {
+    return (HashMap<String, Object>) (new JsonStreamReader(x).readJavaTree());
+  }
+
+  private String str(Object tree) {
+    JsonStreamWriter writer = new JsonStreamWriter();
+    writer.writeTree(tree);
+    return writer.toString();
+  }
+
+  @Test
+  public void values() {
+    HashMap<String, Object> root = of("{}");
+    HashMap<String, Object> simple = of("{\"x\":123,\"y\":42}");
+    JsonDeltaIntegrate.integrateDeltaIntoRoot(root, simple);
+    Assert.assertEquals("{\"x\":123,\"y\":42}", str(root));
+  }
+
+  @Test
+  public void just_obj() {
+    HashMap<String, Object> root = of("{\"x\":123,\"y\":42}");
+    HashMap<String, Object> simple = of("{\"x\":{\"x\":123,\"y\":42},\"y\":{\"x\":123,\"y\":42}}");
+    JsonDeltaIntegrate.integrateDeltaIntoRoot(root, simple);
+    Assert.assertEquals("{\"x\":{\"x\":123,\"y\":42},\"y\":{\"x\":123,\"y\":42}}", str(root));
+  }
+
+  @Test
+  public void delete() {
+    HashMap<String, Object> root = of("{\"x\":123,\"y\":42}");
+    HashMap<String, Object> simple = of("{\"x\":null,\"y\":null}");
+    JsonDeltaIntegrate.integrateDeltaIntoRoot(root, simple);
+    Assert.assertEquals("{}", str(root));
+  }
+
+  @Test
+  public void build_array() {
+    HashMap<String, Object> root = of("{}");
+    JsonDeltaIntegrate.integrateDeltaIntoRoot(root, of("{\"x\":{\"@o\":[\"1\",\"3\",\"2\"],\"1\":{\"x\":42},\"2\":{\"x\":50},\"3\":{\"x\":100}}}"));
+    Assert.assertEquals("{\"x\":[{\"__key\":\"1\",\"x\":42},{\"__key\":\"3\",\"x\":100},{\"__key\":\"2\",\"x\":50}],\"#x\":{\"1\":{\"__key\":\"1\",\"x\":42},\"2\":{\"__key\":\"2\",\"x\":50},\"3\":{\"__key\":\"3\",\"x\":100},\"__key\":\"x\",\"@o\":true}}", str(root));
+
+    JsonDeltaIntegrate.integrateDeltaIntoRoot(root, of("{\"x\":{\"1\":{\"x\":500}}}"));
+    Assert.assertEquals("{\"x\":[{\"__key\":\"1\",\"x\":500},{\"__key\":\"3\",\"x\":100},{\"__key\":\"2\",\"x\":50}],\"#x\":{\"1\":{\"__key\":\"1\",\"x\":500},\"2\":{\"__key\":\"2\",\"x\":50},\"3\":{\"__key\":\"3\",\"x\":100},\"__key\":\"x\",\"@o\":true}}", str(root));
+
+    JsonDeltaIntegrate.integrateDeltaIntoRoot(root, of("{\"x\":{\"@o\":[[0,1]]}}"));
+    Assert.assertEquals("{\"x\":[{\"__key\":\"1\",\"x\":500},{\"__key\":\"3\",\"x\":100}],\"#x\":{\"1\":{\"__key\":\"1\",\"x\":500},\"2\":{\"__key\":\"2\",\"x\":50},\"3\":{\"__key\":\"3\",\"x\":100},\"__key\":\"x\",\"@o\":true}}", str(root));
+
+    JsonDeltaIntegrate.integrateDeltaIntoRoot(root, of("{\"x\":{\"1\":null,\"2\":null,\"3\":null,\"@s\":0}}"));
+    Assert.assertEquals("{\"x\":[],\"#x\":{\"__key\":\"x\",\"@o\":true}}", str(root));
+  }
+
+  @Test
+  public void array_with_values() {
+    HashMap<String, Object> root = of("{}");
+    JsonDeltaIntegrate.integrateDeltaIntoRoot(root, of("{\"x\":{\"@o\":[\"1\",\"3\",\"2\"],\"1\":{\"x\":42},\"2\":{\"x\":50},\"3\":{\"x\":100}}}"));
+    Assert.assertEquals("{\"x\":[{\"__key\":\"1\",\"x\":42},{\"__key\":\"3\",\"x\":100},{\"__key\":\"2\",\"x\":50}],\"#x\":{\"1\":{\"__key\":\"1\",\"x\":42},\"2\":{\"__key\":\"2\",\"x\":50},\"3\":{\"__key\":\"3\",\"x\":100},\"__key\":\"x\",\"@o\":true}}", str(root));
+
+    JsonDeltaIntegrate.integrateDeltaIntoRoot(root, of("{\"x\":{\"1\":5000,\"@o\":[3,2,1]}}"));
+    Assert.assertEquals("{\"x\":[{\"__key\":\"3\",\"x\":100},{\"__key\":\"2\",\"x\":50},5000],\"#x\":{\"1\":5000,\"2\":{\"__key\":\"2\",\"x\":50},\"3\":{\"__key\":\"3\",\"x\":100},\"__key\":\"x\",\"@o\":true}}", str(root));
+
+    JsonDeltaIntegrate.integrateDeltaIntoRoot(root, of("{\"x\":{\"3\":{\"x\":500000},\"@s\":1}}"));
+    Assert.assertEquals("{\"x\":[{\"__key\":\"3\",\"x\":500000}],\"#x\":{\"1\":5000,\"2\":{\"__key\":\"2\",\"x\":50},\"3\":{\"__key\":\"3\",\"x\":500000},\"__key\":\"x\",\"@o\":true}}", str(root));
+  }
+}
