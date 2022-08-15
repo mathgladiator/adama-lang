8189a5f1608d349f7c029d4b4ea4839b4e364d74
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2022-08-15 18:58:32 -0500

    test the runnable and copyright sprinkle

---
+++ web/src/test/java/org/adamalang/web/service/RedirectServiceRunnableTests.java
@@ -0,0 +1,56 @@
+/*
+ * This file is subject to the terms and conditions outlined in the file 'LICENSE' (hint: it's MIT); this file is located in the root directory near the README.md which you should also read.
+ *
+ * This file is part of the 'Adama' project which is a programming language and document store for board games; however, it can be so much more.
+ *
+ * See https://www.adama-platform.com/ for more information.
+ *
+ * (c) 2020 - 2022 by Jeffrey M. Barber ( http://jeffrey.io )
+ */
+package org.adamalang.web.service;
+
+import org.adamalang.common.metrics.NoOpMetricsFactory;
+import org.adamalang.web.service.mocks.MockServiceBase;
+import org.adamalang.web.service.mocks.MockWellKnownHandler;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RedirectServiceRunnableTests {
+
+  @Test
+  public void test_interrupt() throws Exception {
+    WebConfig webConfig = WebConfigTests.mockConfig(WebConfigTests.Scenario.Mock1);
+    final var runnable = new RedirectAndWellknownServiceRunnable(webConfig, new WebMetrics(new NoOpMetricsFactory()), new MockWellKnownHandler(), () -> {});
+    final var thread = new Thread(runnable);
+    thread.start();
+    Assert.assertTrue(runnable.waitForReady(2500));
+    Assert.assertTrue(runnable.isAccepting());
+    thread.interrupt();
+    thread.join();
+    runnable.shutdown();
+  }
+
+  @Test
+  public void test_shutdown() throws Exception {
+    WebConfig webConfig = WebConfigTests.mockConfig(WebConfigTests.Scenario.Mock2);
+    final var runnable = new RedirectAndWellknownServiceRunnable(webConfig, new WebMetrics(new NoOpMetricsFactory()), new MockWellKnownHandler(), () -> {});
+    final var thread = new Thread(runnable);
+    thread.start();
+    Assert.assertTrue(runnable.waitForReady(10000));
+    Assert.assertTrue(runnable.isAccepting());
+    runnable.shutdown();
+    thread.join();
+    runnable.shutdown();
+  }
+
+  @Test
+  public void test_tight_shutdown() throws Exception {
+    WebConfig webConfig = WebConfigTests.mockConfig(WebConfigTests.Scenario.Mock3);
+    final var runnable = new RedirectAndWellknownServiceRunnable(webConfig, new WebMetrics(new NoOpMetricsFactory()), new MockWellKnownHandler(), () -> {});
+    runnable.shutdown();
+    final var thread = new Thread(runnable);
+    thread.start();
+    thread.join();
+    runnable.shutdown();
+  }
+}
