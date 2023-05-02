b549440320968385dc95b9de5acb8815b3a9f2d4
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2023-05-01 20:19:17 -0500

    ok, it's MIT based still

--- core/src/main/java/org/adamalang/runtime/data/DataService.java
+++ core/src/main/java/org/adamalang/runtime/data/DataService.java
@@ -1,9 +1,8 @@
 /*
- * This file is subject to the terms and conditions outlined in the file 'LICENSE' (hint: it's Apache2); this file is located in the root directory near the README.md which you should also read.
- *
- * This file is part of the 'Adama' project which is a programming language and document store for board games; however, it can be so much more.
- *
- * See https://www.adama-platform.com/ for more information.
+ * This file is subject to the terms and conditions outlined in the
+ * file 'LICENSE' (hint: it's MIT-based) located in the root directory
+ * near the README.md which you should also read. For more information
+ * about the project which owns this file, see https://www.adama-platform.com/ .
  *
  * (c) 2020 - 2023 by Jeffrey M. Barber ( http://jeffrey.io )
  */
