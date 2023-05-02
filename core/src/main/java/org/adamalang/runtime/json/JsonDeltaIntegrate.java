c84981200fb6ebbfe1ad0e242fdc7034e200d9e1
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2023-05-01 20:10:22 -0500

    upgrade copyright for 2023

--- core/src/main/java/org/adamalang/runtime/json/JsonDeltaIntegrate.java
+++ core/src/main/java/org/adamalang/runtime/json/JsonDeltaIntegrate.java
@@ -1,11 +1,11 @@
 /*
- * This file is subject to the terms and conditions outlined in the file 'LICENSE' (hint: it's MIT); this file is located in the root directory near the README.md which you should also read.
+ * This file is subject to the terms and conditions outlined in the file 'LICENSE' (hint: it's Apache2); this file is located in the root directory near the README.md which you should also read.
  *
  * This file is part of the 'Adama' project which is a programming language and document store for board games; however, it can be so much more.
  *
  * See https://www.adama-platform.com/ for more information.
  *
- * (c) 2020 - 2022 by Jeffrey M. Barber ( http://jeffrey.io )
+ * (c) 2020 - 2023 by Jeffrey M. Barber ( http://jeffrey.io )
  */
 package org.adamalang.runtime.json;
 
