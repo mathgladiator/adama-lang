319ab839f637aa751d6e5e75a937404d1ec3bddf
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2023-05-01 20:16:19 -0500

    Update DefaultCopyright.java

--- common/src/main/java/org/adamalang/common/DefaultCopyright.java
+++ common/src/main/java/org/adamalang/common/DefaultCopyright.java
@@ -10,12 +10,12 @@
 package org.adamalang.common;
 
 public class DefaultCopyright {
-  public static final String COPYRIGHT_FILE_PREFIX = "/*\n" + //
-      " * This file is subject to the terms and conditions outlined in the file 'LICENSE' (hint: it's Apache2); this file is located in the root directory near the README.md which you should also read.\n" + //
-      " *\n" + //
-      " * This file is part of the 'Adama' project which is a programming language and document store for board games; however, it can be so much more.\n" + //
-      " *\n" + //
-      " * See https://www.adama-platform.com/ for more information.\n" + //
+  public static final String COPYRIGHT_FILE_PREFIX =
+      "/*\n" + //
+      " * This file is subject to the terms and conditions outlined in the\n" +
+      " * file 'LICENSE' (hint: it's MIT-based) located in the root directory\n" +
+      " * near the README.md which you should also read. For more information\n" +
+      " * about the project which owns this file, see https://www.adama-platform.com/ .\n" + //
       " *\n" + //
       " * (c) 2020 - 2023 by Jeffrey M. Barber ( http://jeffrey.io )\n" + " */\n";
 }
