8a33f13fac0a94e7cd4eb7c2305a6e19d757aa5e
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2023-06-24 10:05:24 -0500

    Update copyright

--- common/src/main/java/org/adamalang/common/DefaultCopyright.java
+++ common/src/main/java/org/adamalang/common/DefaultCopyright.java
@@ -1,10 +1,10 @@
 /*
  * This file is subject to the terms and conditions outlined in the
- * file 'LICENSE' (hint: it's MIT-based) located in the root directory
+ * file 'LICENSE' (it's dual licensed) located in the root directory
  * near the README.md which you should also read. For more information
  * about the project which owns this file, see https://www.adama-platform.com/ .
  *
- * (c) 2020 - 2023 by Jeffrey M. Barber ( http://jeffrey.io )
+ * (c) 2021 - 2023 by Adama Platform Initiative, LLC
  */
 package org.adamalang.common;
 
@@ -12,9 +12,9 @@ public class DefaultCopyright {
   public static final String COPYRIGHT_FILE_PREFIX =
       "/*\n" + //
       " * This file is subject to the terms and conditions outlined in the\n" +
-      " * file 'LICENSE' (hint: it's MIT-based) located in the root directory\n" +
+      " * file 'LICENSE' (it's dual licensed) located in the root directory\n" +
       " * near the README.md which you should also read. For more information\n" +
       " * about the project which owns this file, see https://www.adama-platform.com/ .\n" + //
       " *\n" + //
-      " * (c) 2020 - 2023 by Jeffrey M. Barber ( http://jeffrey.io )\n" + " */\n";
+      " * (c) 2021 - 2023 by Adama Platform Initiative, LLC\n" + " */\n";
 }
