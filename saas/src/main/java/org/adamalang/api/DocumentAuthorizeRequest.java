1da5e413a109dbb183fa450d36dc27de443c8fce
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2023-07-30 18:38:53 -0500

    data plane doesn't need space resolver

--- saas/src/main/java/org/adamalang/api/DocumentAuthorizeRequest.java
+++ saas/src/main/java/org/adamalang/api/DocumentAuthorizeRequest.java
@@ -13,7 +13,6 @@ import org.adamalang.common.Callback;
 import org.adamalang.common.ErrorCodeException;
 import org.adamalang.common.NamedRunnable;
 import org.adamalang.frontend.Session;
-import org.adamalang.transforms.results.SpacePolicy;
 import org.adamalang.validators.ValidateKey;
 import org.adamalang.validators.ValidateSpace;
 import org.adamalang.web.io.*;
@@ -21,14 +20,12 @@ import org.adamalang.web.io.*;
 /** Authorize a username and password against a document. */
 public class DocumentAuthorizeRequest {
   public final String space;
-  public final SpacePolicy policy;
   public final String key;
   public final String username;
   public final String password;
 
-  public DocumentAuthorizeRequest(final String space, final SpacePolicy policy, final String key, final String username, final String password) {
+  public DocumentAuthorizeRequest(final String space, final String key, final String username, final String password) {
     this.space = space;
-    this.policy = policy;
     this.key = key;
     this.username = username;
     this.password = password;
@@ -36,16 +33,18 @@ public class DocumentAuthorizeRequest {
 
   public static void resolve(Session session, RegionConnectionNexus nexus, JsonRequest request, Callback<DocumentAuthorizeRequest> callback) {
     try {
-      final BulkLatch<DocumentAuthorizeRequest> _latch = new BulkLatch<>(nexus.executor, 1, callback);
       final String space = request.getStringNormalize("space", true, 461828);
       ValidateSpace.validate(space);
-      final LatchRefCallback<SpacePolicy> policy = new LatchRefCallback<>(_latch);
       final String key = request.getString("key", true, 466947);
       ValidateKey.validate(key);
       final String username = request.getString("username", true, 458737);
       final String password = request.getString("password", true, 465917);
-      _latch.with(() -> new DocumentAuthorizeRequest(space, policy.get(), key, username, password));
-      nexus.spaceService.execute(session, space, policy);
+      nexus.executor.execute(new NamedRunnable("documentauthorize-success") {
+        @Override
+        public void execute() throws Exception {
+           callback.success(new DocumentAuthorizeRequest(space, key, username, password));
+        }
+      });
     } catch (ErrorCodeException ece) {
       nexus.executor.execute(new NamedRunnable("documentauthorize-error") {
         @Override
@@ -58,7 +57,6 @@ public class DocumentAuthorizeRequest {
 
   public void logInto(ObjectNode _node) {
     _node.put("space", space);
-    org.adamalang.transforms.SpacePolicyLocator.logInto(policy, _node);
     _node.put("key", key);
   }
 }
