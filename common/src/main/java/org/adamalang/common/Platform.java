e2075125fc85a5e5e253b2501b1bd92beb67b2d1
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2023-08-14 06:53:24 -0500

    cleanup after regen

--- common/src/main/java/org/adamalang/common/Platform.java
+++ common/src/main/java/org/adamalang/common/Platform.java
@@ -9,5 +9,5 @@
 package org.adamalang.common;
 
 public class Platform {
-  public static final String VERSION = "20230814063338";
+  public static final String VERSION = "20230814065209";
 }
--- core/src/main/java/org/adamalang/runtime/contracts/DeleteTask.java
+++ core/src/main/java/org/adamalang/runtime/contracts/DeleteTask.java
@@ -1,30 +1,25 @@
+/*
+ * This file is subject to the terms and conditions outlined in the
+ * file 'LICENSE' (it's dual licensed) located in the root directory
+ * near the README.md which you should also read. For more information
+ * about the project which owns this file, see https://www.adama-platform.com/ .
+ *
+ * (c) 2021 - 2023 by Adama Platform Initiative, LLC
+ */
 package org.adamalang.runtime.contracts;
 
 import org.adamalang.common.Callback;
-import org.adamalang.common.ErrorCodeException;
 
+/** a task to wedge between mark-delete and commit-delete */
 public interface DeleteTask {
 
   public void executeAfterMark(Callback<Void> callback);
 
+  /** trivial task that just executes the callback; i.e. do nothing */
   public static final DeleteTask TRIVIAL = new DeleteTask() {
     @Override
     public void executeAfterMark(Callback<Void> callback) {
       callback.success(null);
     }
   };
-
-  public static Callback<Void> SKIP(DeleteTask task, Callback<Void> callback) {
-    return new Callback<Void>() {
-      @Override
-      public void success(Void value) {
-        task.executeAfterMark(callback);
-      }
-
-      @Override
-      public void failure(ErrorCodeException ex) {
-        callback.failure(ex);
-      }
-    };
-  }
 }
