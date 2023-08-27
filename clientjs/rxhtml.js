46307f3b868d380587e8f917e274a03ad7fbc7bc
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2023-08-27 13:56:39 -0500

    polish on debugger, connection icon works without tailwind, devbox supports anonymous agents

--- clientjs/rxhtml.js
+++ clientjs/rxhtml.js
@@ -9,7 +9,7 @@ var RxHTML = (function () {
 
   var connectionMonitorDom = document.createElement("div");
   connection.onstatuschange = function (status) {
-    var icon = function (color) { return "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"" + color + "\" class=\"w-6 h-6\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z\" /></svg>\n"; };
+    var icon = function (color) { return "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"" + color + "\" style=\"width:16px; height:16px\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z\" /></svg>\n"; };
     if (!status) {
       connectionMonitorDom.title = "disconnected";
       connectionMonitorDom.innerHTML = icon("red");
---
+++ net/src/main/java/org/adamalang/net/client/LocalFinder.java
@@ -0,0 +1,13 @@
+package org.adamalang.net.client;
+
+import org.adamalang.common.Callback;
+import org.adamalang.runtime.data.DocumentLocation;
+import org.adamalang.runtime.data.Key;
+import org.adamalang.runtime.data.SimpleFinderService;
+
+public class LocalFinder implements SimpleFinderService {
+  @Override
+  public void find(Key key, Callback<DocumentLocation> callback) {
+
+  }
+}
