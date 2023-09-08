1dc59890f0075b45f73cecbd6932701c6be2cbc6
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2023-09-08 12:16:00 -0500

    regen

--- common/src/main/java/org/adamalang/common/RequestLogger.java
+++ common/src/main/java/org/adamalang/common/RequestLogger.java
@@ -8,6 +8,7 @@
  */
 package org.adamalang.common;
 
-public class Platform {
-  public static final String VERSION = "20230908105435";
+/** simple contract to define how to log requests; this is for monitoring both first and third party services */
+public interface RequestLogger<In, Out> {
+  public Callback<Out> wrap(In request, Callback<Out> callback);
 }
