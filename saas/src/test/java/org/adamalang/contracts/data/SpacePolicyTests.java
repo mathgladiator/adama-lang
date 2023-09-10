0d1e7d681c7aaf9da2142dd0653f7ab4edb64d7e
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2023-09-10 18:58:39 -0500

    implement a very basic policy for authorities
    I like it. Need to optimize, but it's good enough for now

---
+++ saas/src/test/java/org/adamalang/contracts/data/SpacePolicyTests.java
@@ -0,0 +1,97 @@
+package org.adamalang.contracts.data;
+
+import org.adamalang.mysql.data.SpaceInfo;
+import org.adamalang.runtime.natives.NtPrincipal;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.TreeSet;
+
+public class SpacePolicyTests {
+
+  private static SpacePolicy of(String policy) {
+    TreeSet<Integer> developers = new TreeSet<>();
+    developers.add(4);
+    SpaceInfo info = new SpaceInfo(1, 2, developers, true, 42, policy);
+    return new SpacePolicy(info);
+  }
+  public static AuthenticatedUser simple(int id) {
+    return new AuthenticatedUser(id, new NtPrincipal(id + "", "adama"), null);
+  }
+
+  public static AuthenticatedUser simple(String agent, String authority) {
+    return new AuthenticatedUser(-1, new NtPrincipal(agent, authority), null);
+  }
+
+  @Test
+  public void defaults() {
+    SpacePolicy policy = of("{}");
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.Owner, simple(2)));
+    Assert.assertFalse(policy.checkPolicy("x", DefaultPolicyBehavior.Owner, simple(4)));
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple(2)));
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple(4)));
+    Assert.assertFalse(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple(3)));
+  }
+
+  @Test
+  public void defaults_x() {
+    SpacePolicy policy = of("{\"x\":{}}");
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.Owner, simple(2)));
+    Assert.assertFalse(policy.checkPolicy("x", DefaultPolicyBehavior.Owner, simple(4)));
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple(2)));
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple(4)));
+    Assert.assertFalse(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple(3)));
+  }
+
+  @Test
+  public void defaults_bs() {
+    SpacePolicy policy = of("{\"x\":null}");
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.Owner, simple(2)));
+    Assert.assertFalse(policy.checkPolicy("x", DefaultPolicyBehavior.Owner, simple(4)));
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple(2)));
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple(4)));
+    Assert.assertFalse(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple(3)));
+  }
+
+  @Test
+  public void defaults_null_authorities() {
+    SpacePolicy policy = of("{\"allowed-authorities\":null}");
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.Owner, simple(2)));
+    Assert.assertFalse(policy.checkPolicy("x", DefaultPolicyBehavior.Owner, simple(4)));
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple(2)));
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple(4)));
+    Assert.assertFalse(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple(3)));
+  }
+
+  @Test
+  public void defaults_bad_authorities() {
+    SpacePolicy policy = of("{\"allowed-authorities\":{}}}");
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.Owner, simple(2)));
+    Assert.assertFalse(policy.checkPolicy("x", DefaultPolicyBehavior.Owner, simple(4)));
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple(2)));
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple(4)));
+    Assert.assertFalse(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple(3)));
+  }
+
+  @Test
+  public void developers_on_owner_default() {
+    SpacePolicy policy = of("{\"x\":{\"developers\":true}}");
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.Owner, simple(2)));
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.Owner, simple(4)));
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple(2)));
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple(4)));
+    Assert.assertFalse(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple(3)));
+  }
+
+  @Test
+  public void provide_authority() {
+    SpacePolicy policy = of("{\"x\":{\"allowed-authorities\":[1, \"cake\", false, null]}}");
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.Owner, simple(2)));
+    Assert.assertFalse(policy.checkPolicy("x", DefaultPolicyBehavior.Owner, simple(4)));
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple(2)));
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple(4)));
+    Assert.assertFalse(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple(3)));
+    Assert.assertTrue(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple("x", "cake")));
+    Assert.assertFalse(policy.checkPolicy("x", DefaultPolicyBehavior.OwnerAndDevelopers, simple("x", "ninja")));
+  }
+}
