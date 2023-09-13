462038180202e67f1b39ae18777090ad7420ffdc
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2023-09-12 22:56:16 -0500

    polish a typing rule

--- core/src/main/java/org/adamalang/translator/tree/types/checking/ruleset/RuleSetIterable.java
+++ core/src/main/java/org/adamalang/translator/tree/types/checking/ruleset/RuleSetIterable.java
@@ -24,7 +24,7 @@ public class RuleSetIterable {
         return true;
       }
       if (!silent) {
-        environment.document.createError(tyTypeOriginal, String.format("Type check failure: must be either an array, list, or map to use foreach; instead got '%s'.", tyTypeOriginal.getAdamaType()));
+        environment.document.createError(tyTypeOriginal, String.format("Type check failure: must be either an array, list, or map to use foreach or index lookup; instead got '%s'.", tyTypeOriginal.getAdamaType()));
       }
     }
     return false;
--- core/src/test/java/org/adamalang/translator/GeneratedLookupTests.java
+++ core/src/test/java/org/adamalang/translator/GeneratedLookupTests.java
@@ -86,7 +86,7 @@ public class GeneratedLookupTests extends GeneratedBase {
     StringBuilder gold = new StringBuilder();
     gold.append("Path:Lookup_ByIndexNotIterable_failure.a");
     gold.append("\n--ISSUES-------------------------------------------");
-    gold.append("\n[{\"range\":{\"start\":{\"line\":1,\"character\":2,\"byte\":15},\"end\":{\"line\":1,\"character\":12,\"byte\":25}},\"severity\":1,\"source\":\"error\",\"message\":\"Type check failure: must be either an array, list, or map to use foreach; instead got 'int'.\",\"file\":\"./test_code/Lookup_ByIndexNotIterable_failure.a\"}]\"--JAVA---------------------------------------------");
+    gold.append("\n[{\"range\":{\"start\":{\"line\":1,\"character\":2,\"byte\":15},\"end\":{\"line\":1,\"character\":12,\"byte\":25}},\"severity\":1,\"source\":\"error\",\"message\":\"Type check failure: must be either an array, list, or map to use foreach or index lookup; instead got 'int'.\",\"file\":\"./test_code/Lookup_ByIndexNotIterable_failure.a\"}]\"--JAVA---------------------------------------------");
     gold.append("\n");
     gold.append("\nFailedValidation");
     assertStable(live, gold);
