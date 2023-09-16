b3344d2a807cdc303e5de07658afc8dfeb4f7a25
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2023-09-16 10:56:50 -0500

    change license to AGPL

--- common/src/main/java/org/adamalang/common/DefaultCopyright.java
+++ common/src/main/java/org/adamalang/common/DefaultCopyright.java
@@ -1,17 +1,23 @@
 /*
- * This file is subject to the terms and conditions outlined in the
- * file 'LICENSE' (it's dual licensed) located in the root directory
- * near the README.md which you should also read. For more information
- * about the project which owns this file, see https://www.adama-platform.com/ .
- *
- * (c) 2021 - 2023 by Adama Platform Initiative, LLC
- */
+* Adama Platform and Language
+* Copyright (C) 2021 - 2023 by Adama Platform Initiative, LLC
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+* 
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 package org.adamalang.common;
 
 /** standard copyright header for all Adama Platform assets in this repo */
 public class DefaultCopyright {
-  public static final String COPYRIGHT_FILE_PREFIX = "/*\n" + //
-      " * This file is subject to the terms and conditions outlined in the\n" + " * file 'LICENSE' (it's dual licensed) located in the root directory\n" + " * near the README.md which you should also read. For more information\n" + " * about the project which owns this file, see https://www.adama-platform.com/ .\n" + //
-      " *\n" + //
-      " * (c) 2021 - 2023 by Adama Platform Initiative, LLC\n" + " */\n";
+  public static final String COPYRIGHT_FILE_PREFIX = "/*\n" + "* Adama Platform and Language\n" + "* Copyright (C) 2021 - 2023 by Adama Platform Initiative, LLC\n" + "* \n" + "* This program is free software: you can redistribute it and/or modify\n" + "* it under the terms of the GNU Affero General Public License as published\n" + "* by the Free Software Foundation, either version 3 of the License, or\n" + "* (at your option) any later version.\n" + "* \n" + "* This program is distributed in the hope that it will be useful,\n" + "* but WITHOUT ANY WARRANTY; without even the implied warranty of\n" + "* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" + "* GNU Affero General Public License for more details.\n" + "* \n" + "* You should have received a copy of the GNU Affero General Public License\n" + "* along with this program.  If not, see <https://www.gnu.org/licenses/>.\n" + "*/\n";
 }
--- web/src/main/java/org/adamalang/web/assets/cache/MemoryCacheAsset.java
+++ web/src/main/java/org/adamalang/web/assets/cache/MemoryCacheAsset.java
@@ -1,11 +1,20 @@
 /*
- * This file is subject to the terms and conditions outlined in the
- * file 'LICENSE' (it's dual licensed) located in the root directory
- * near the README.md which you should also read. For more information
- * about the project which owns this file, see https://www.adama-platform.com/ .
- *
- * (c) 2021 - 2023 by Adama Platform Initiative, LLC
- */
+* Adama Platform and Language
+* Copyright (C) 2021 - 2023 by Adama Platform Initiative, LLC
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+* 
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 package org.adamalang.web.assets.cache;
 
 import org.adamalang.common.NamedRunnable;
