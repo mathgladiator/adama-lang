213a0377a04ce3b0081ae5a61dd379846b781b30
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2023-11-05 17:42:06 -0600

    Update Platform.java

--- common/src/main/java/org/adamalang/common/Platform.java
+++ common/src/main/java/org/adamalang/common/Platform.java
@@ -18,6 +18,6 @@
 package org.adamalang.common;
 
 public class Platform {
-  public static final String VERSION = "20231105171759";
+  public static final String VERSION = "20231105174202";
   public static final String JS_VERSION = "367f626ca4bdf8c5126bb1e79e3c113e";
 }
