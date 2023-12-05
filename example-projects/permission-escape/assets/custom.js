2d6a1b4765d6a94f4675c952bf9ae1182dc73de1
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2023-12-05 08:57:44 -0600

    update example to not depend on attribute values

--- example-projects/permission-escape/assets/custom.js
+++ example-projects/permission-escape/assets/custom.js
@@ -3,19 +3,11 @@ window.rxhtml.PRWP('checkbox_hierarchy', function (dom, state, rxobj, maker, fra
   maker(dom, state);
 
   // we are going to index the children DOM into a logical structure to provide the behavior semantics that we want
-  var permission_groups = {};
-
-  // since everything is indexed by a concept of a group, this function will get[orCreate] the group structure
-  var getGroup = function(name) {
-    if (!(name in permission_groups)) {
-      permission_groups[name] = {name:name, children:[]};
-    }
-    return permission_groups[name];
-  }
+  var permission_groups = {next:0};
 
   // various changes will require a re-sync, and this will bring the DOM consistent
   var sync = function() {
-    for(var gid in permission_groups) {
+    for (var gid = 1; gid <= permission_groups.next; gid++) {
       var grp = permission_groups[gid];
       var c_true = 0;
       var c_false = 0;
@@ -36,9 +28,8 @@ window.rxhtml.PRWP('checkbox_hierarchy', function (dom, state, rxobj, maker, fra
     }
   };
 
-  var index_aspects = function(dom) {
-    if (dom.hasAttribute('showhide:toggle')) {
-      var grp = getGroup(dom.getAttribute('showhide:toggle'));
+  var index_aspects = function(dom, grp) {
+    if (dom.hasAttribute('group:header')) {
       grp.toggle = dom;
       dom.addEventListener('click', function() {
         if (this.target.style.display == "none") {
@@ -49,17 +40,14 @@ window.rxhtml.PRWP('checkbox_hierarchy', function (dom, state, rxobj, maker, fra
       }.bind(grp));
     }
     if (dom.hasAttribute("group:summary")) {
-      var grp = getGroup(dom.getAttribute('group:summary'));
       grp.summary = dom;
     }
-    if (dom.hasAttribute('showhide:target')) {
-      var grp = getGroup(dom.getAttribute('showhide:target'));
+    if (dom.hasAttribute('group:body')) {
       grp.target = dom;
       dom.addEventListener('click', function() {
       }.bind(grp));
     }
-    if (dom.hasAttribute("check:group")) {
-      var grp = getGroup(dom.getAttribute('check:group'));
+    if (dom.hasAttribute("group:all")) {
       grp.all = dom;
       dom.addEventListener('change', function() {
         for (var k = 0; k < this.children.length; k++) {
@@ -68,25 +56,30 @@ window.rxhtml.PRWP('checkbox_hierarchy', function (dom, state, rxobj, maker, fra
         sync();
       }.bind(grp));
     }
-    if (dom.hasAttribute("part:of")) {
-      var grp = getGroup(dom.getAttribute('part:of'));
+    if (dom.hasAttribute("group:child")) {
       grp.children.push(dom);
       dom.addEventListener('change', function() {
         sync();
       }.bind(grp));
-      // TODO: RxHTML needs a forcechange event
     }
 
+    var new_grp = grp;
+    if (dom.tagName.toUpperCase() == "PERMISSION-GROUP") {
+      new_grp = {children:[]};
+      permission_groups.next++;
+      permission_groups[permission_groups.next] = new_grp;
+      console.log("WR:" + permission_groups.next);
+    }
     if ("children" in dom) {
       var arr = dom.children;
       var n = arr.length;
       for (var k = 0; k < n; k++) {
-        index_aspects(dom.children[k]);
+        index_aspects(dom.children[k], new_grp);
       }
     }
   };
 
-  index_aspects(dom);
+  index_aspects(dom, null);
 
   sync();
 });
