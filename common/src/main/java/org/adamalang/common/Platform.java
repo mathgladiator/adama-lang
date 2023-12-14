eec70542513c96880af13662b37d647ddb5f20c1
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2023-12-14 12:07:44 -0600

    regen js and delete a dash of dead code

--- common/src/main/java/org/adamalang/common/Platform.java
+++ common/src/main/java/org/adamalang/common/Platform.java
@@ -18,6 +18,6 @@
 package org.adamalang.common;
 
 public class Platform {
-  public static final String VERSION = "20231214120253";
-  public static final String JS_VERSION = "b031c537ee45a3968f2ff0a728e0de4d";
+  public static final String VERSION = "20231214120723";
+  public static final String JS_VERSION = "3b85b99d500d7affddbdeb6fb4efc5c6";
 }
