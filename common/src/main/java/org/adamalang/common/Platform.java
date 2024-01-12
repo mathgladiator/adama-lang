b850b99ca798b5aa46878976dee94b60e6576556
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2024-01-11 21:49:31 -0600

    regen

--- common/src/main/java/org/adamalang/common/Platform.java
+++ common/src/main/java/org/adamalang/common/Platform.java
@@ -18,6 +18,6 @@
 package org.adamalang.common;
 
 public class Platform {
-  public static final String VERSION = "20240111211649";
+  public static final String VERSION = "20240111214916";
   public static final String JS_VERSION = "2109c168fe8f858901c5f2bd78d7bfce";
 }
