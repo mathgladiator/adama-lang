e49b71047867868dd008d6f53db34d0b7654cfb7
Dark Lord of Infrastructure <48896921+mathgladiator@users.noreply.github.com>
2024-01-22 16:10:57 -0600

    more tests to confirm behavior of new features

---
+++ core/src/test/java/org/adamalang/runtime/sys/ServiceCronTests.java
@@ -0,0 +1,78 @@
+/*
+* Adama Platform and Language
+* Copyright (C) 2021 - 2023 by Adama Platform Initiative, LLC
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+* 
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+package org.adamalang.runtime.sys;
+
+import org.adamalang.common.Callback;
+import org.adamalang.common.TimeSource;
+import org.adamalang.common.metrics.NoOpMetricsFactory;
+import org.adamalang.runtime.ContextSupport;
+import org.adamalang.runtime.LivingDocumentTests;
+import org.adamalang.runtime.data.Key;
+import org.adamalang.runtime.data.RemoteDocumentUpdate;
+import org.adamalang.runtime.data.UpdateType;
+import org.adamalang.runtime.json.JsonStreamReader;
+import org.adamalang.runtime.mocks.MockTime;
+import org.adamalang.runtime.natives.NtPrincipal;
+import org.adamalang.runtime.remote.Deliverer;
+import org.adamalang.runtime.sys.mocks.*;
+import org.adamalang.translator.jvm.LivingDocumentFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ServiceCronTests {
+  private static final CoreMetrics METRICS = new CoreMetrics(new NoOpMetricsFactory());
+  private static final Key KEY = new Key("space", "key");
+  private static final String SIMPLE_CRON =
+      "@static { create { return true; } } public int x; @connected { x = 42; return @who == @no_one; } @cron fooz daily 8:00 { x += 100; }";
+
+  @Test
+  public void cron_happy() throws Exception {
+    LivingDocumentFactory factory = LivingDocumentTests.compile(SIMPLE_CRON, Deliverer.FAILURE);
+    MockInstantLivingDocumentFactoryFactory factoryFactory =
+        new MockInstantLivingDocumentFactoryFactory(factory);
+    MockTime time = new MockTime();
+    MockInstantDataService dataService = new MockInstantDataService();
+    CoreService service = new CoreService(METRICS, factoryFactory, (bill) -> {},  new MockMetricsReporter(), dataService, time, 3);
+    try {
+      NullCallbackLatch created = new NullCallbackLatch();
+      service.create(ContextSupport.WRAP(NtPrincipal.NO_ONE), KEY, "{}", null, created);
+      created.await_success();
+
+      MockStreamback streamback = new MockStreamback();
+      Runnable latch1 = streamback.latchAt(2);
+      Runnable latch2 = streamback.latchAt(3);
+      Runnable latch3 = streamback.latchAt(5);
+      service.connect(ContextSupport.WRAP(NtPrincipal.NO_ONE), KEY, "{}", streamback);
+      streamback.await_began();
+      latch1.run();
+      Assert.assertEquals("STATUS:Connected", streamback.get(0));
+      Assert.assertEquals("{\"data\":{\"x\":42},\"seq\":5}", streamback.get(1));
+      time.time += 1000L * 60L * 60L * 24 * 2;
+      service.invalidateAll();
+      latch2.run();
+      Assert.assertEquals("{\"data\":{\"x\":142},\"seq\":6}", streamback.get(2));
+      time.time += 1000L * 60L * 60L * 24 * 2;
+      service.invalidateAll();
+      latch3.run();
+      Assert.assertEquals("{\"seq\":7}", streamback.get(3)); // ok, why?
+      Assert.assertEquals("{\"data\":{\"x\":242},\"seq\":8}", streamback.get(4));
+    } finally {
+      service.shutdown();
+    }
+  }
+}
---
+++ core/src/test/java/org/adamalang/runtime/sys/ServiceEnqueueTests.java
@@ -0,0 +1,74 @@
+/*
+* Adama Platform and Language
+* Copyright (C) 2021 - 2023 by Adama Platform Initiative, LLC
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+* 
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+package org.adamalang.runtime.sys;
+
+import org.adamalang.common.metrics.NoOpMetricsFactory;
+import org.adamalang.runtime.ContextSupport;
+import org.adamalang.runtime.LivingDocumentTests;
+import org.adamalang.runtime.data.Key;
+import org.adamalang.runtime.mocks.MockTime;
+import org.adamalang.runtime.natives.NtPrincipal;
+import org.adamalang.runtime.remote.Deliverer;
+import org.adamalang.runtime.sys.mocks.*;
+import org.adamalang.translator.jvm.LivingDocumentFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ServiceEnqueueTests {
+  private static final CoreMetrics METRICS = new CoreMetrics(new NoOpMetricsFactory());
+  private static final Key KEY = new Key("space", "key");
+  private static final String SIMPLE_CRON =
+      "@static { create { return true; } } public int x; @connected { x = 42; return @who == @no_one; } @cron fooz daily 8:00 { x += 7; doit.enqueue(@no_one, {}); } message M {} channel doit(M m) { x += 100; } ";
+
+  @Test
+  public void enqueue_transfer_happy() throws Exception {
+    LivingDocumentFactory factory = LivingDocumentTests.compile(SIMPLE_CRON, Deliverer.FAILURE);
+    MockInstantLivingDocumentFactoryFactory factoryFactory =
+        new MockInstantLivingDocumentFactoryFactory(factory);
+    MockTime time = new MockTime();
+    MockInstantDataService dataService = new MockInstantDataService();
+    CoreService service = new CoreService(METRICS, factoryFactory, (bill) -> {},  new MockMetricsReporter(), dataService, time, 3);
+    try {
+      NullCallbackLatch created = new NullCallbackLatch();
+      service.create(ContextSupport.WRAP(NtPrincipal.NO_ONE), KEY, "{}", null, created);
+      created.await_success();
+
+      MockStreamback streamback = new MockStreamback();
+      Runnable latch1 = streamback.latchAt(2);
+      Runnable latch2 = streamback.latchAt(4);
+       Runnable latch3 = streamback.latchAt(6);
+      service.connect(ContextSupport.WRAP(NtPrincipal.NO_ONE), KEY, "{}", streamback);
+      streamback.await_began();
+      latch1.run();
+      Assert.assertEquals("STATUS:Connected", streamback.get(0));
+      Assert.assertEquals("{\"data\":{\"x\":42},\"seq\":5}", streamback.get(1));
+      time.time += 1000L * 60L * 60L * 24 * 2;
+      service.invalidateAll();
+      latch2.run();
+      Assert.assertEquals("{\"data\":{\"x\":49},\"seq\":6}", streamback.get(2));
+      Assert.assertEquals("{\"data\":{\"x\":149},\"seq\":8}", streamback.get(3));
+      time.time += 1000L * 60L * 60L * 24 * 2;
+      service.invalidateAll();
+      latch3.run();
+      Assert.assertEquals("{\"data\":{\"x\":156},\"seq\":9}", streamback.get(4)); // ok, why?
+      Assert.assertEquals("{\"data\":{\"x\":256},\"seq\":11}", streamback.get(5));
+    } finally {
+      service.shutdown();
+    }
+  }
+}
