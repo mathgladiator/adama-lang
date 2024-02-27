08928250973d8a018d54ab874eae2f3fbce433cb
Jay Hostetter <jhostetter@homeadvisor.com>
2024-02-27 14:51:00 -0600

    typo

--- clientjs/rxcapacitor.js
+++ clientjs/rxcapacitor.js
@@ -62,7 +62,7 @@ async function LinkCapacitor($, identityName) {
     if (!localStorage.getItem("mdo_host")) {
       $.registerManifest(`${url.origin}/~d/.product-manifest.json`, true);
     }
-    if (url.parthname > 1) {
+    if (url.pathname > 1) {
       window.rxhtml.goto(url.pathname, true);
     }
   });
